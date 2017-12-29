1dcd5e897702e04c3d5268a94a9fa7e441c75a54
kanaabe <kanakoabe5@gmail.com>
2017-12-29 02:57:45 -0500

    Add image inversion

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,11 +3,7 @@ import Link from 'gatsby-link'
 
 const IndexPage = () => (
   <div
-    style={{
-      background: 'url("https://s3.amazonaws.com/kana-assets/bg.jpg")',
-      backgroundSize: 'cover',
-      height: 'calc(100vh - 8.45rem)'
-    }}
+    className='index__main-image'
   />
 )
 
