9826de3049fad4084383d0ce148540f9d0276a52
Kanako Abe <kanakoabe5@gmail.com>
2018-11-08 18:27:57 -0500

    Dependency migrations for Gatsby V2

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,17 @@
-import React from 'react'
-import Link from 'gatsby-link'
+import React from "react"
+import styled from "styled-components"
 
-const IndexPage = () => (
-  <div
-    className='index__main-image'
-  />
-)
+const IndexPage = () => {
+  return (
+    <div>
+      <div className="index__main-image" />
+      {/* <Canvas width="100vw" height="100vh" /> */}
+    </div>
+  )
+}
+
+const Canvas = styled.canvas`
+  background-color: transparent;
+`
 
 export default IndexPage
