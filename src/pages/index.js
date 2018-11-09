dd980313e9c06a9e0d9c069d07b6fbfbab99cefa
Kanako Abe <kanakoabe5@gmail.com>
2018-11-09 15:38:09 -0500

    An animation with drawings arr

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,17 +2,22 @@ import React from "react"
 import styled from "styled-components"
 import Layout from "../components/layout"
 
+const TRANSLATE_INCR = 1
+
 export default class IndexPage extends React.Component {
   state = {
     isDrawing: false,
     lastX: 0,
-    lastY: 0
+    lastY: 0,
+    currentDrawing: [],
+    drawings: []
   }
 
   constructor(props) {
     super(props)
     this.canvas = React.createRef()
     this.ctx = null
+    this.frame = null
   }
 
   componentDidMount() {
@@ -21,62 +26,110 @@ export default class IndexPage extends React.Component {
       this.ctx.lineJoin = "round"
       this.ctx.lineCap = "round"
       this.ctx.lineWidth = 8
-      this.ctx.strokeStyle = "#576070"
+      this.ctx.strokeStyle = "#eabe2e"
+      this.ctx.fillStyle = "#000"
+      this.frame = setInterval(this.draw, 60)
     }
   }
 
-  shouldComponentUpdate() {
-    // Avoid re-rendering on any state change
-    return false
+  componentWillUnmount() {
+    clearInterval(this.frame)
   }
 
   handleMouseDown = e => {
     this.setState({
       isDrawing: true,
       lastX: e.clientX,
-      lastY: e.clientY - 100
+      lastY: e.clientY - 100,
+      currentDrawing: []
     })
+    clearInterval(this.frame)
+    this.ctx.beginPath()
   }
 
   handleMouseUp = () => {
-    this.setState({ isDrawing: false })
-    console.log(this.state)
+    const { drawings, currentDrawing } = this.state
+    this.setState({
+      isDrawing: false,
+      drawings: [...drawings, currentDrawing],
+      currentDrawing: []
+    })
+
+    this.ctx.closePath()
+    this.ctx.stroke()
+    this.frame = setInterval(this.draw, 60)
+    console.log(this.state.drawings)
   }
 
   handleMouseMove = e => {
-    if (this.state.isDrawing && this.ctx) {
-      let { lastX, lastY } = this.state
-      this.ctx.beginPath()
+    let { lastX, lastY, isDrawing, currentDrawing } = this.state
+    if (isDrawing && this.ctx) {
       this.ctx.moveTo(lastX, lastY)
       this.ctx.lineTo(e.clientX, e.clientY - 100)
       this.ctx.stroke()
-
-      console.log({
-        "e.clientX": e.clientX,
-        "e.clientY": e.clientY,
-        lastX,
-        lastY
-      })
-
       this.setState({
         lastX: e.clientX,
-        lastY: e.clientY - 100
+        lastY: e.clientY - 100,
+        currentDrawing: [...currentDrawing, [lastX, lastY]]
       })
+    }
+  }
+
+  draw = () => {
+    const { drawings } = this.state
+    if (this.ctx && drawings.length) {
+      let newDrawings = []
+      this.ctx.clearRect(
+        0,
+        0,
+        this.canvas.current.width,
+        this.canvas.current.height
+      )
+
+      drawings.forEach(drawing => {
+        let newDrawing = drawing.map(([x, y]) => [x, y + TRANSLATE_INCR])
+        let maxY = 0
+        this.ctx.beginPath()
+        for (let i = 0; i < newDrawing.length - 1; i++) {
+          // set new points
+          let x = newDrawing[i][0]
+          let y = newDrawing[i][1]
+          let nextX = newDrawing[i + 1][0]
+          let nextY = newDrawing[i + 1][1]
+
+          maxY = Math.max(maxY, y)
+
+          // draw line
+          this.ctx.moveTo(x, y)
+          this.ctx.lineTo(nextX, nextY)
+        }
+        this.ctx.closePath()
+        this.ctx.stroke()
+        if (maxY < window.innerHeight && newDrawing.length) {
+          newDrawings.push(newDrawing)
+        }
+      })
+      this.setState({ drawings: newDrawings })
     }
   }
 
   render() {
     return (
       <>
+        <Layout>
+          <div
+            className={`index__main-image ${
+              this.state.isDrawing ? "isDrawing" : ""
+            }`}
+          />
+          <span style={{ float: "right" }}>draw something.</span>
+        </Layout>
         <Canvas
           ref={this.canvas}
           handleMouseDown={this.handleMouseDown}
           handleMouseUp={this.handleMouseUp}
           handleMouseMove={this.handleMouseMove}
         />
-        <Layout>
-          <div className="index__main-image" />
-        </Layout>
       </>
     )
   }
@@ -104,5 +157,4 @@ const StyledCanvas = styled.canvas`
   left: 0;
   margin-top: 100px;
   box-sizing: border-box;
-  cursor: url("favicon.png");
 `
